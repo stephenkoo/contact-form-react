28be6df6641e2ed67cb2b190324fdf2de7bf353b
Stephen Koo <stephenyrkoo@gmail.com>
2017-10-02 22:27:06 +1100

    Add form fields, labels, and submit button

--- web/src/components/Form.js
+++ web/src/components/Form.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 const Form = () => (
   <form>
-    Hello world!
+    <div>
+      <label htmlFor="email">Your email address</label>
+      <input name="email" id="email" placeholder="Your email"/>
+    </div>
+    <div>
+      <label htmlFor="message">Your message</label>
+      <textarea name="message" id="message" placeholder="Your message"/>
+    </div>
+    <button type="submit">Submit</button>
   </form>
 );
 
