da7019cbf12fcdb95f0354d2bc8721c64de13959
Stephen Koo <stephenyrkoo@gmail.com>
2017-10-03 01:07:03 +1100

    Add email validation and formFilled validation to onSubmit function

--- web/src/container/FormContainer.js
+++ web/src/container/FormContainer.js
@@ -26,8 +26,33 @@ class FormContainer extends PureComponent {
   logState = e => {
     e.preventDefault();
 
-    console.log('State:', this.state);
+    const validForm = this.validateForm(this.state.email, this.state.message);
+
+    if (validForm) {
+      console.log('Successfully posted:', this.state);
+    }
   }
+
+  validateForm = (email, message) => {
+    const formFilled = email !== '' && message !== '';
+    const validEmail = this.validateEmail(email);
+
+    try {
+      if (!validEmail) throw new Error('Please enter a valid email address.');
+      if (!formFilled) throw new Error('Please fill in the email and message fields.');
+
+      return true;
+    }
+    catch(err) {
+      console.log(err);
+      return false;
+    }
+  }
+
+  validateEmail = email => {
+    const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/igm;
+    return emailRegex.test(email);
+  };
 
   render() {
     return (
