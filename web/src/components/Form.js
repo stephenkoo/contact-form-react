5eed181bdc87d9ba68f094f2dc790801d1a6aa97
Stephen Koo <stephenyrkoo@gmail.com>
2017-10-04 01:37:18 +1100

    Use 'classnames' package to apply conditional styling to notifications based on notificationType

--- web/src/components/Form.js
+++ web/src/components/Form.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
 import './form.css';
 
 const Form = ({
@@ -8,35 +10,43 @@ const Form = ({
   handleChange,
   handleSubmit,
   notificationType
-}) => (
-  <form className="contact-form" onSubmit={handleSubmit}>
-    <div className="contact-form-group">
-      <label htmlFor="email">Your email address</label>
-      <input
-        name="email"
-        id="email"
-        placeholder="Your email"
-        type="email"
-        value={emailValue}
-        onChange={handleChange}
-      />
-    </div>
-    <div className="contact-form-group">
-      <label htmlFor="message">Your message</label>
-      <textarea
-        name="message"
-        id="message"
-        placeholder="Your message"
-        value={messageValue}
-        onChange={handleChange}
-      />
-    </div>
-    <button type="submit">Submit</button>
-    {notificationType &&
-      <div>{notificationType}</div>
-    }
-  </form>
-);
+}) => {
+  const notificationClasses = classNames (
+    'contact-form-notification',
+    {'contact-form-notification-failure' : notificationType === 'failed' },
+    {'contact-form-notification-success' : notificationType === 'submitted' }
+  );
+
+  return (
+    <form className="contact-form" onSubmit={handleSubmit}>
+      <div className="contact-form-group">
+        <label htmlFor="email">Your email address</label>
+        <input
+          name="email"
+          id="email"
+          placeholder="Your email"
+          type="email"
+          value={emailValue}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="contact-form-group">
+        <label htmlFor="message">Your message</label>
+        <textarea
+          name="message"
+          id="message"
+          placeholder="Your message"
+          value={messageValue}
+          onChange={handleChange}
+        />
+      </div>
+      <button type="submit">Submit</button>
+      {notificationType &&
+        <div class={notificationClasses}>{notificationType}</div>
+      }
+    </form>
+  )
+};
 
 Form.propTypes = {
   emailValue: PropTypes.string.isRequired,
